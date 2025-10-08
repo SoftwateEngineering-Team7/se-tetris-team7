af9ef63e0ddc9a5eaaf2435a102f7cec4f5a0155
KimDaegyu <ddk3207@naver.com>
2025-10-08 21:12:53 +0900

    Revert "feat: MVC 팩토리, 번들 구현"
    This reverts commit 7911406b304ffce11cbae252f4ac1e5698799c30.

--- app/src/main/java/org/tetris/shared/MvcBundle.java
+++
@@ -1,6 +0,0 @@
-package org.tetris.shared;
-
-/** 묶어서 돌려주는 DTO */
-public record MvcBundle<M extends BaseModel,
-C extends BaseController<M, V>,
-V extends BaseView>(M model, C controller, V view) {}
