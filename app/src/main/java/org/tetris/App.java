74b36627c23e29a8b503e7531112523b32c4d521
KimDaegyu <ddk3207@naver.com>
2025-10-09 17:07:48 +0900

    feat: UI 라우터 구현

--- app/src/main/java/org/tetris/App.java
+++ app/src/main/java/org/tetris/App.java
@@ -1,6 +1,5 @@
 package org.tetris;
 
-import org.tetris.menu.start.controller.StartUIRouter;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -8,7 +7,7 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) {
-        StartUIRouter nav = new StartUIRouter(stage);
+        Router nav = new Router(stage);
         nav.showStartMenu(); // 시작 화면 표시
         stage.show();
     }
