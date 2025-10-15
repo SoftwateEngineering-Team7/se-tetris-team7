9ff5cf552b1f0cd98f66a8cca35557e20a375e2b
ZiLA3 <93520847+ZiLA3@users.noreply.github.com>
2025-10-15 20:28:27 +0900

    Feat: Block color, Block Test (#27)
    * feat: blockColor
    
    * feat: BlockTest
    
    * chore: unusable import
    
    * GetLength() -> setLength()
    
    * chore: resolve conflict
    
    ---------
    
    Co-authored-by: KimDaegyu <136952602+KimDaegyu-Dev@users.noreply.github.com>

--- app/src/main/java/org/tetris/game/model/blocks/ZBlock.java
+++ app/src/main/java/org/tetris/game/model/blocks/ZBlock.java
@@ -1,13 +1,20 @@
 package org.tetris.game.model.blocks;
 
+import org.util.GameColor;
 import org.util.Point;
 
 public class ZBlock extends Block {
+
+    private static final int[][] Z_SHAPE = {
+        { 7, 7, 0 },
+        { 0, 7, 7 }
+    };
+    
+    private static final Point Z_POINT = new Point(1, 1);
+
+    private static final GameColor Z_COLOR = GameColor.CYAN;
+
     public ZBlock() {
-        shape = new int[][] {
-                { 7, 7, 0 },
-                { 0, 7, 7 }
-        };
-        pivot = new Point(1, 1);
+        super(Z_SHAPE, Z_POINT, Z_COLOR);
     }
 }
