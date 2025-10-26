9e4c7b56e4436cc6521a0cfcac4d39d0d88a6b1b
ZiLA3 <93520847+ZiLA3@users.noreply.github.com>
2025-10-26 15:59:01 +0900

    feat: Block reShape 추가

---
+++ app/src/main/java/org/tetris/game/model/blocks/ConcreteBlock.java
@@ -0,0 +1,12 @@
+package org.tetris.game.model.blocks;
+
+import org.util.GameColor;
+import org.util.Point;
+
+public class ConcreteBlock extends Block {
+
+    public ConcreteBlock(int[][] shape, Point pivot, GameColor color) {
+        super(shape, pivot, color);
+    }
+
+}
