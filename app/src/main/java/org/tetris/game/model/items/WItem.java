2d5338fe65c7f34b74a1ecf2c47d2c18460d5299
ZiLA3 <93520847+ZiLA3@users.noreply.github.com>
2025-10-26 17:38:24 +0900

    fix: Test Error Fix

--- app/src/main/java/org/tetris/game/model/items/WItem.java
+++ app/src/main/java/org/tetris/game/model/items/WItem.java
@@ -5,10 +5,15 @@ import org.tetris.game.model.blocks.*;
 import org.util.Point;
 
 public class WItem extends Item {
+
     private static final int[][] W_SHAPE = {
             { 0, 10, 10, 0 },
             { 10, 10, 10, 10 }
     };
+
+    protected WItem() {
+        super(10);
+    }
 
     @Override
     public Block GetItemBlock(Block block) {
@@ -19,8 +24,7 @@ public class WItem extends Item {
 
     @Override
     public void Activate(Board board) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Activate'");
+        
     }
 
 }
