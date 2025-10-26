45c55d163072f137c49c22d6517519446c3b883f
ZiLA3 <93520847+ZiLA3@users.noreply.github.com>
2025-10-26 15:59:34 +0900

    feat: Item 추상 Class, LItem 추가

---
+++ app/src/main/java/org/tetris/game/model/items/Item.java
@@ -0,0 +1,17 @@
+package org.tetris.game.model.items;
+
+import org.tetris.game.model.blocks.Block;
+import org.util.Point;
+
+public abstract class Item {
+
+    protected Point position;
+
+    public abstract Block GetItemBlock(Block block);
+
+    public abstract void Activate();
+
+    public Point getPosition() {
+        return position;
+    }
+}
---
+++ app/src/test/java/org/tetris/game/model/ItemTest.java
@@ -0,0 +1,27 @@
+package org.tetris.game.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import org.tetris.game.model.blocks.*;
+import org.tetris.game.model.items.*;
+import org.util.Point;
+
+public class ItemTest {
+    
+    @Test
+    public void testLItemGetItemBlock() {
+        Block block = new ZBlock();
+        Item item = new LItem();
+
+        Block itemBlock = item.GetItemBlock(block);
+        Point itemPos = item.getPosition();
+
+        System.out.println("Item Block\n" + itemBlock);
+        System.out.println("Item Position: " + itemPos);
+
+        int itemValue = itemBlock.getShape(itemPos.r, itemPos.c);
+        assertTrue(itemValue == 9);
+    }
+}
