c6a6f60ef4e21d49e8579613e39eff24e8714cf1
Park Jae Hong <154869827+H00ong@users.noreply.github.com>
2025-10-29 23:57:56 +0900

    Feat : Block 확률 리스트 반환 매서드 추가 (#59)
    * fix : 난이도 확률 리스트 반환 함수 수정
    
    * test : 확률 리스트 함수 추가, 기존 test 함수 값 수정

--- app/src/test/java/org/util/DifficultyTest.java
+++ app/src/test/java/org/util/DifficultyTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.tetris.game.model.NextBlockModel;
 
 public class DifficultyTest {
     
@@ -24,8 +25,8 @@ public class DifficultyTest {
         Difficulty.setCurrentDifficulty(Difficulty.EASY_STRING);
         assertEquals("현재 난이도는 EASY여야 합니다", 
                     Difficulty.EASY_STRING, Difficulty.getCurrentDifficulty());
-        assertEquals("EASY의 속도 배율은 1.0이어야 합니다", 
-                    1.0, Difficulty.getSpeedMultiplier(), 0.001);
+        assertEquals("EASY의 속도 배율은 0.8이어야 합니다", 
+                    0.8, Difficulty.getSpeedMultiplier(), 0.001);
     }
     
     @Test
@@ -33,8 +34,8 @@ public class DifficultyTest {
         Difficulty.setCurrentDifficulty(Difficulty.NORMAL_STRING);
         assertEquals("현재 난이도는 NORMAL이어야 합니다", 
                     Difficulty.NORMAL_STRING, Difficulty.getCurrentDifficulty());
-        assertEquals("NORMAL의 속도 배율은 1.5여야 합니다", 
-                    1.5, Difficulty.getSpeedMultiplier(), 0.001);
+        assertEquals("NORMAL의 속도 배율은 1.0이어야 합니다", 
+                    1.0, Difficulty.getSpeedMultiplier(), 0.001);
     }
     
     @Test
@@ -42,7 +43,32 @@ public class DifficultyTest {
         Difficulty.setCurrentDifficulty(Difficulty.HARD_STRING);
         assertEquals("현재 난이도는 HARD여야 합니다", 
                     Difficulty.HARD_STRING, Difficulty.getCurrentDifficulty());
-        assertEquals("HARD의 속도 배율은 2.0이어야 합니다", 
-                    2.0, Difficulty.getSpeedMultiplier(), 0.001);
+        assertEquals("HARD의 속도 배율은 1.2이어야 합니다", 
+                    1.2, Difficulty.getSpeedMultiplier(), 0.001);
     }
+
+    @Test
+    public void testGetEasyProbList(){
+        Difficulty.setCurrentDifficulty(Difficulty.EASY_STRING);
+        int[] easyProb = Difficulty.getBlockProbList();
+        assertArrayEquals("EASY 난이도의 확률 리스트와 같아야 합니다.", 
+                          NextBlockModel.EASY_BLOCK_PROB_LIST, easyProb);
+    }
+
+    @Test
+    public void testGetNormalProbList(){
+        Difficulty.setCurrentDifficulty(Difficulty.NORMAL_STRING);
+        int[] normalProb = Difficulty.getBlockProbList();
+        assertArrayEquals("NORMAL 난이도의 확률 리스트와 같아야 합니다.", 
+                          NextBlockModel.DEFAULT_BLOCK_PROB_LIST, normalProb);
+    }
+
+    @Test
+    public void testGetHardProbList(){
+        Difficulty.setCurrentDifficulty(Difficulty.HARD_STRING);
+        int[] hardProb = Difficulty.getBlockProbList();
+        assertArrayEquals("HARD 난이도의 확률 리스트와 같아야 합니다.", 
+                          NextBlockModel.HARD_BLOCK_PROB_LIST, hardProb);
+    }
+
 }
