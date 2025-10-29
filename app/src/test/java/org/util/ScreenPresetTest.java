0b08a0e0d4b012637cf0816836ba3821bddd2963
박재홍 <pjhbbogri@gmail.com>
2025-10-29 15:19:20 +0900

    chore : 스크린 사이즈 변경

--- app/src/test/java/org/util/ScreenPresetTest.java
+++ app/src/test/java/org/util/ScreenPresetTest.java
@@ -24,8 +24,8 @@ public class ScreenPresetTest {
         ScreenPreset.setCurrentPreset(ScreenPreset.SMALL_STRING);
         assertEquals("현재 프리셋은 SMALL이어야 합니다", 
                     ScreenPreset.SMALL_STRING, ScreenPreset.getCurrentPreset());
-        assertEquals("SMALL의 너비는 800이어야 합니다", 800, ScreenPreset.getWidth());
-        assertEquals("SMALL의 높이는 600이어야 합니다", 600, ScreenPreset.getHeight());
+        assertEquals("SMALL의 너비는 800이어야 합니다", ScreenPreset.SMALL_WIDTH, ScreenPreset.getWidth());
+        assertEquals("SMALL의 높이는 600이어야 합니다", ScreenPreset.SMALL_HEIGHT, ScreenPreset.getHeight());
     }
     
     @Test
@@ -33,8 +33,8 @@ public class ScreenPresetTest {
         ScreenPreset.setCurrentPreset(ScreenPreset.MIDDLE_STRING);
         assertEquals("현재 프리셋은 MIDDLE이어야 합니다", 
                     ScreenPreset.MIDDLE_STRING, ScreenPreset.getCurrentPreset());
-        assertEquals("MIDDLE의 너비는 1000이어야 합니다", 1000, ScreenPreset.getWidth());
-        assertEquals("MIDDLE의 높이는 700이어야 합니다", 700, ScreenPreset.getHeight());
+        assertEquals("MIDDLE의 너비는 1000이어야 합니다", ScreenPreset.MIDDLE_WIDTH, ScreenPreset.getWidth());
+        assertEquals("MIDDLE의 높이는 700이어야 합니다", ScreenPreset.MIDDLE_HEIGHT, ScreenPreset.getHeight());
     }
     
     @Test
@@ -42,7 +42,7 @@ public class ScreenPresetTest {
         ScreenPreset.setCurrentPreset(ScreenPreset.LARGE_STRING);
         assertEquals("현재 프리셋은 LARGE여야 합니다", 
                     ScreenPreset.LARGE_STRING, ScreenPreset.getCurrentPreset());
-        assertEquals("LARGE의 너비는 1200이어야 합니다", 1200, ScreenPreset.getWidth());
-        assertEquals("LARGE의 높이는 800이어야 합니다", 800, ScreenPreset.getHeight());
+        assertEquals("LARGE의 너비는 1200이어야 합니다", ScreenPreset.LARGE_WIDTH, ScreenPreset.getWidth());
+        assertEquals("LARGE의 높이는 800이어야 합니다", ScreenPreset.LARGE_HEIGHT, ScreenPreset.getHeight());
     }
 }
