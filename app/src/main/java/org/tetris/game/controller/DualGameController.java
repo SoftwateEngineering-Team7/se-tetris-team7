6e99b97bdd2185f6bd6a1d1998da35fb8de32537
강형석 <fluor.kang@gmail.com>
2025-12-04 03:45:09 +0900

    fix: private -> protected

--- app/src/main/java/org/tetris/game/controller/DualGameController.java
+++ app/src/main/java/org/tetris/game/controller/DualGameController.java
@@ -91,13 +91,13 @@ public class DualGameController<M extends DualGameModel> extends BaseController<
 
     // Player 1 Control Labels
     @FXML
-    private Label moveControlLabel1;
+    protected Label moveControlLabel1;
     @FXML
-    private Label rotateControlLabel1;
+    protected Label rotateControlLabel1;
     @FXML
-    private Label softDropControlLabel1;
+    protected Label softDropControlLabel1;
     @FXML
-    private Label hardDropControlLabel1;
+    protected Label hardDropControlLabel1;
 
     // Player 2 Control Labels
     @FXML
